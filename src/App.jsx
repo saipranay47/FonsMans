2c107b27970861c1faad424006886ec65af65771
saipranay47 <saipranayadepu07@gmail.com>
2024-01-04 16:10:56 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import data from "./assets/data.json";
+
 function App() {
   return (
     <div className="min-h-screen bg-black text-white p-4 grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8">
@@ -12,7 +14,7 @@ function App() {
 
           <div className="font-light">
             <p className="text-lg">Hello, I'm Fons Mans.</p>
-            <p className="opacity-50 max-w-[300px] ">
+            <p className="opacity-50 max-w-[310px] ">
               I build my ideas, help others to shape theirs, and talk about
               everything design.
             </p>
@@ -20,49 +22,93 @@ function App() {
         </div>
       </div>
       <div className="rounded-lg lg:col-span-2 max-lg:justify-between font-light">
-        <div className="p-8 pl-4 -mt-2 flex w-full max-md:justify-between">
-          <span className="lg:max-w-xs md:w-full">
+        <div className="p-8 pl-4 -mt-2 flex w-full max-md:justify-between flex-col sm:flex-row gap-2">
+          <span className="lg:max-w-[300px] md:w-full max-sm:flex justify-between items-center">
             <p className="text-lg">Got a question?</p>
-            <p className="opacity-50 leading-3">Get in touch</p>
+            <a href="#" className="opacity-50 ">
+              Get in touch
+            </a>
           </span>
-          <span className="lg:max-w-xs md:w-full">
+          <span className="lg:max-w-xs md:w-full max-sm:flex justify-between items-center">
             <p className="text-lg">Stay in loop</p>
-            <p className="opacity-50 leading-3">Subscribe</p>
+            <a href="#" className="opacity-50 ">
+              Subscribe
+            </a>
           </span>
-          <span className="lg:max-w-xs md:w-full">
+          <span className="lg:max-w-xs md:w-full max-sm:flex justify-between items-center">
             <p className="text-lg">I'm on socials</p>
-            <p className="opacity-50 leading-3">Follow me on x.com</p>
+            <a href="#" className="opacity-50">
+              Follow me on x.com
+            </a>
           </span>
         </div>
 
-        <div className="p-8 pl-4 mt-60 flex w-full max-lg:justify-between max-md:flex-wrap">
-          <div className="max-w-[300px] w-full">
-            <div className="w-full max-w-[200px]">
+        <div className="p-8 pl-4 md:mt-60 flex w-full max-lg:justify-center max-lg:items-start flex-wrap gap-y-20 gap-x-28 lg:gap-y-10">
+          <div className="md:max-w-[200px] w-full">
+            <div className="w-full md:max-w-[200px]">
               <p className="text-lg">Projects</p>
-              <span className="opacity-50 flex justify-between w-full">
-                <p className="">10x Designers</p>
-                <p>'22</p>
-              </span>
+              {data.projects.map((project) => (
+                <span className="flex justify-between w-full my-1">
+                  <p className="">
+                    <span className="opacity-50">{project.name}</span>
+                    {project.new && (
+                      <span class="whitespace-nowrap rounded-md bg-[#331908] px-[6px] leading-0 ml-2 py-[3px] text-xs font-normal text-orange-600">
+                        new
+                      </span>
+                    )}
+                  </p>
+                  <p className="opacity-50">'{project.year}</p>
+                </span>
+              ))}
             </div>
           </div>
-          <div className="max-w-[300px] w-full">
-            <div className="w-full max-w-[200px]">
-              <p className="text-lg">Projects</p>
-              <span className="opacity-50 flex justify-between w-full">
-                <p className="">10x Designers</p>
-                <p>'22</p>
-              </span>
+          <div className="md:max-w-[210px] w-full lg:mb-10">
+            <div className="w-full md:max-w-[210px]">
+              <p className="text-lg">Stack</p>
+              {data.stack.map((stack) => (
+                <span className="opacity-50 flex justify-between w-full my-1">
+                  <p className="">{stack}</p>
+                  <p>Web design</p>
+                </span>
+              ))}
             </div>
           </div>
-          <div className="max-w-[300px] w-full">
-            <div className="w-full max-w-[200px]">
-              <p className="text-lg">Projects</p>
-              <span className="opacity-50 flex justify-between w-full">
-                <p className="">10x Designers</p>
-                <p>'22</p>
-              </span>
+          <div className="md:max-w-[220px] w-full">
+            <div className="w-full md:max-w-[220px]">
+              <p className="text-lg">Channels</p>
+              {data.channels.map((channel) => (
+                <span className="opacity-50 flex justify-between w-full my-1">
+                  <p className="">{channel.name}</p>
+                  <p>{channel.type}</p>
+                </span>
+              ))}
             </div>
           </div>
+          <div className="md:max-w-[200px] w-full lg:-mt-14">
+            <div className="w-full md:max-w-[220px]">
+              <p className="text-lg">Features</p>
+              {data.features.map((features) => (
+                <span className="opacity-50 flex justify-between w-full my-1">
+                  <p className="">{features.name}</p>
+                  <p>'{features.year}</p>
+                </span>
+              ))}
+            </div>
+          </div>
+          <div className="md:max-w-[210px] w-full">
+            <div className="w-full md:max-w-[220px]">
+              <p className="text-lg">Clients</p>
+              {data.clients.map((clients) => (
+                <span className="opacity-50 flex justify-between w-full my-1">
+                  <p className="">{clients.name}</p>
+                  <p>{clients.type}</p>
+                </span>
+              ))}
+            </div>
+          </div>
+        <button class="bg-orange-600 hover:bg-orange-700 text-black font-bold py-3 px-6 rounded-full lg:absolute right-10 bottom-20">
+          Book a session
+        </button>
         </div>
       </div>
     </div>
